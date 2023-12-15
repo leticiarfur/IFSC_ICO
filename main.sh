ff5e632d964bcf6468e377bc587d73a7ac1af317
Diego <diego.s281@aluno.ifsc.edu.br>
2023-12-15 20:30:43 -0300

    teste quase final

--- main.sh
+++ main.sh
@@ -1,2 +1,7 @@
 echo Diego, Eduardo, Henrique, Leticia e Sophia
-bash henrique-patricio.sh
+./diego-sfoggia.sh
+if -d existe pasta diego-sfoggia
+then echo disponível
+else echo não disponível
+fi
+
